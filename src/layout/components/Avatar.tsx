1ba90052d75b141ab9120bfd49260b3a5f3ba984
AlwanN01 <alwannabawi112233@gmail.com>
2023-04-05 09:11:04 +0700

    add memo

--- src/layout/components/Avatar.tsx
+++ src/layout/components/Avatar.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image'
+import { memo } from 'react'
 
 type Props = {}
 function Avatar({}: Props) {
+  console.log('Avatar')
+
   return <Image className='rounded-full' height={30} width={30} alt='Avatar' src={'/images/placeholder.jpg'} />
 }
-export default Avatar
+export default memo(Avatar)
