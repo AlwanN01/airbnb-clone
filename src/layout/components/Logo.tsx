e4fa5d5387ff3da836fb693ee98d9fa3ac4001e0
AlwanN01 <alwannabawi112233@gmail.com>
2023-04-10 01:32:37 +0700

    fix search

--- src/layout/components/Logo.tsx
+++ src/layout/components/Logo.tsx
@@ -5,6 +5,6 @@ import { useRouter } from 'next/navigation'
 type Props = {}
 function Logo({}: Props) {
   const router = useRouter()
-  return <Image alt='Logo' className='hidden cursor-pointer md:block' height='100' width='100' src='/images/logo.png' />
+  return <Image alt='Logo' className='hidden cursor-pointer pr-2 md:block' height='100' width='100' src='/images/logo.png' />
 }
 export default Logo
